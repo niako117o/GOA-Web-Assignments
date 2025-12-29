71e248fd3f41e4a0ba17198c797d81c9dcd880d5
niako117 <niasimonia12345@gmail.com>
2025-12-29 18:47:07 +0400

    commit

---
+++ level 062/homework/main.js
@@ -0,0 +1,36 @@
+// 3) გამოიტანე კონსოლში შემდეგი მოქმედებების პასუხები:
+//       10 - 3 
+//       4 × 6
+//       20 ÷ 5
+console.log(10 - 3);
+console.log(4 * 6);
+console.log(20 / 5);
+
+
+
+// 4) კონსოლში დაბეჭდე შენი სახელი და გვარი
+
+console.log("nia simonia");
+
+// 5) გააკეთეთ თითო მათემატიკურ ოპერაციაზე 3-3 მაგალითი, სასურველია რომ ყველა მათემატიკურ ოპერაციაში იყოს გამოყენებული რიცხვი 10
+console.log(10 - 3);
+console.log(10 - 4);
+console.log(10 - 7);
+
+console.log(10 + 10);
+console.log(10 + 5);
+console.log(10 + 9);
+
+console.log(10 / 2);
+console.log(10 / 5);
+console.log(10 / 10);
+
+console.log(10 * 3);
+console.log(10 * 7);
+console.log(10 * 12);
+
+console.log(10 % 2);
+console.log(10 % 5);
+console.log(10 % 10);
+
+
